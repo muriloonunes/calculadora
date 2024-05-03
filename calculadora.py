b6d456e6d9f986951bf75defc91d48517fd956dd
murilo <murilo_no@outlook.com>
2024-05-03 14:08:50 -0300

    exceptions handled

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,32 @@
 import sys
 import math 
 
-"__version__ == 1.0"
+"__version__ == 1.1"
 
+while True:
+    try:
+        n1 = float(input("Digite o primeiro numero\n").strip())
+        break
+    except ValueError as e:
+        print("numero invalido")
+        
+while True:
+    try: 
+        n2 = float(input("Digite o segundo numero\n").strip())
+        break
+    except ValueError as e:
+        print("numero invalido")
+        
+while True:
+    operacao = input("Digite a operação que você quer realizar\n").strip().lower()
+    if operacao in ["soma", "+", "sum", "subtracao", "-", "difference", "dif", "sub",
+                    "multiplicacao", "*", "product", "mul", "divisao", "/", "quotient", "div",
+                    "potencia", "**", "power"]:
+        break
+    else:
+        print("Operação inválida")
 
-n1 = float(input("Digite o primeiro numero\n").strip())
-n2 = float(input("Digite o segundo numero\n").strip())
-operacao = input("Digite a operacao que voce quer realizar\n").strip()
-
-if n1 % 1 == 0 and n2 %1 ==0:
+if n1 % 1 == 0 and n2 % 1 == 0:
     n1 = int(n1)
     n2 = int(n2)
     
@@ -19,13 +37,15 @@ elif operacao=="subtracao" or operacao=="-" or operacao=="difference" or operaca
     resultado = n1-n2
     print(f"{n1}-{n2} =",resultado)
 elif operacao=="multiplicacao" or operacao=="*" or operacao=="product" or operacao=="mul":
-    resultado = round(n1*n2)
-    print(f"{n1}*{n2} ={resultado:.3f}")
+    resultado = (n1*n2)
+    print(f"{n1} * {n2} = {resultado:.3f}")
 elif operacao=="divisao" or operacao=="/"or operacao=="quotient" or operacao=="div":
-    resultado = round(n1/n2)
-    print(f"{n1}/{n2} =",resultado)
+    try:
+        resultado = (n1/n2)
+        print(f"{n1}/{n2} =",resultado)
+    except ZeroDivisionError as e:
+        print("não é possível dividir por 0")
+        sys.exit(1)
 elif operacao=="potencia" or operacao=="**" or operacao=="power":
     resultado=n1**n2
     print(resultado)
-else:
-    print("Invalid operation")
