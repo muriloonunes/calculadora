33cdab274cae9b21ac431a8be1c9d31f0581a3ab
murilo <murilo_no@outlook.com>
2024-05-04 13:53:43 -0300

    improved source code

--- calculadora.py
+++ calculadora.py
@@ -1,27 +1,25 @@
 import sys
 import math 
 
-"__version__ == 1.1"
+__version__ == "1.1.1"
 
 while True:
     try:
-        n1 = float(input("Digite o primeiro numero\n").strip())
+        n1 = float(input("Digite o primeiro numero\n"))
         break
     except ValueError as e:
         print("numero invalido")
         
 while True:
     try: 
-        n2 = float(input("Digite o segundo numero\n").strip())
+        n2 = float(input("Digite o segundo numero\n"))
         break
     except ValueError as e:
         print("numero invalido")
         
 while True:
-    operacao = input("Digite a operação que você quer realizar\n").strip().lower()
-    if operacao in ["soma", "+", "sum", "subtracao", "-", "difference", "dif", "sub",
-                    "multiplicacao", "*", "product", "mul", "divisao", "/", "quotient", "div",
-                    "potencia", "**", "power"]:
+    operacao = input("Digite a operação que você quer realizar (+, -, *, /, **)\n")
+    if operacao in ["+","-","*", "/", "**",]:
         break
     else:
         print("Operação inválida")
@@ -30,22 +28,22 @@ if n1 % 1 == 0 and n2 % 1 == 0:
     n1 = int(n1)
     n2 = int(n2)
     
-if operacao=="soma" or operacao=="+" or operacao=="sum":
+if operacao=="+":
     resultado = n1+n2
-    print(f"{n1}+{n2} =",resultado)
-elif operacao=="subtracao" or operacao=="-" or operacao=="difference" or operacao=="dif" or operacao=="sub":
+    print(f"{n1} + {n2} = ",resultado)
+elif operacao=="-":
     resultado = n1-n2
-    print(f"{n1}-{n2} =",resultado)
-elif operacao=="multiplicacao" or operacao=="*" or operacao=="product" or operacao=="mul":
+    print(f"{n1} - {n2} = ",resultado)
+elif operacao=="*":
     resultado = (n1*n2)
     print(f"{n1} * {n2} = {resultado:.3f}")
-elif operacao=="divisao" or operacao=="/"or operacao=="quotient" or operacao=="div":
+elif operacao=="/":
     try:
         resultado = (n1/n2)
-        print(f"{n1}/{n2} =",resultado)
+        print(f"{n1} / {n2} = ",resultado)
     except ZeroDivisionError as e:
         print("não é possível dividir por 0")
         sys.exit(1)
-elif operacao=="potencia" or operacao=="**" or operacao=="power":
+elif operacao=="**":
     resultado=n1**n2
     print(resultado)
